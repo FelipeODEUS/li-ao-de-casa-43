8012ee6566f8350f6d90213a18ec304041a852bf
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-01-20 15:35:41 -0300

    caca ao tesouro

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+
+    text("C U T N I F O N", 700,140);
+    text("Dica: Realiza uma tarefa particular!!", 700, 170);
+
+    text("A T E D A S B A", 100,350);
+    text("Dica: Armazena todas as informações!!", 100, 370);
+    
+
+}
--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ function preload() {
  
   
   bg= loadImage("aladdin_cave.jpg")
-  //carregar imagem para o plano de fundo do tesouro
   bg2 = loadImage("treasure.jpg")
 }
 
@@ -26,8 +25,6 @@ function draw() {
   textSize(20);
   fill("white");
   text("Pontuação: " + score, 450, 50);
-// adicione código para alterar o plano de fundo para o plano de fundo do tesouro
-  
 
   if(score === 3) {
     clear()
